b6c6e79adc48f7c31d8d351c5b6ea122d1e81111
Baluthegoat <141105500+Baluthegoat@users.noreply.github.com>
2024-05-03 20:14:48 +0600

    updated search function

---
+++ details.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const pokemonDetails = JSON.parse(localStorage.getItem('pokemonDetails'));
+    if (pokemonDetails) {
+        displayPokemonDetails(pokemonDetails);
+    } else {
+        alert('Pokemon details not found!');
+        window.location.href = 'index.html';
+    }
+});
+
+function displayPokemonDetails(pokemon) {
+    const pokemonName = document.getElementById('pokemon-name');
+    const pokemonImage = document.getElementById('pokemon-image');
+    const pokemonDetailsContainer = document.getElementById('pokemon-details');
+
+    pokemonName.textContent = pokemon.name;
+    pokemonImage.src = pokemon.sprites.front_default;
+
+    const types = pokemon.types.map(type => type.type.name).join(', ');
+    const strength = pokemon.base_experience;
+    const hp = pokemon.stats[0].base_stat;
+
+    pokemonDetailsContainer.innerHTML = `
+        <p><b>Type:</b> <span>${types}</span></p>
+        <p><b>Strength:</b> <span>${strength}</span></p>
+        <p><b>HP:</b> <span>${hp}</span></p>
+       
+    `;
+}
